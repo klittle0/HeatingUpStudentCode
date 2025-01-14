4213c234a635281fdbb5cc765002313d606a002a
kate <kate.little@menloschool.org>
2025-01-13 17:42:19 -0800

    1/13 - first draft

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date |    Time     |                                                                                                                                                                                                                        Update |
+|:-----|:-----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 1/13 | 2:40-3:00pm | I brainstormed with the class to come up with an algorithm. We decided to use a recursive technique similar to Moksha Patam and create a 2nd array that tracks the length of the sequence up until a certain day/temperature. |
+| 1/13 | 4:45-5:30pm |                                                                                                  I wrote pseudocode then coded my first rough implementation. Currently, my program returns a length of 0 for all test cases. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -3,7 +3,7 @@
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Kate Little
  */
 
 public class WeatherPatterns {
@@ -16,7 +16,44 @@ public class WeatherPatterns {
      */
     public static int longestWarmingTrend(int[] temperatures) {
         // TODO: Write your code here!
+        int length = temperatures.length;
+        int[] spanUpTo = new int[length];
+        // Represents the highest temperature that has serves as the start of a span of days
+        int startMax = -10000;
+        // Each position/temp in the array should be a potential start
+        for (int i = 0; i < length; i++){
+            // Only start there if it's lower than the highest previous start
+            if (temperatures[i] < startMax){
+                recurseSpanLength(i, 0, spanUpTo, temperatures);
+            }
+        }
+        int maxRun = 0;
+        for (int span : spanUpTo){
+            if (span > maxRun){
+                maxRun = span;
+            }
+        }
+        return maxRun;
+    }
 
-        return 0;
+    // Calculates the distance it takes to get to a certain temp
+    // Follows an algorithm like moksha patam!
+    public static void recurseSpanLength(int index, int spanLength, int[] spanUpTo, int[] temperatures){
+        // Base case: if final temperature has been reached
+        int length = temperatures.length;
+        if (index == length - 1){
+            return;
+        }
+        // Only update span length if it's greater than what's currently listed
+        if (spanLength > spanUpTo[index]){
+            spanUpTo[index] = spanLength;
+        }
+        // Recurse for each subsequent spot on the board, but only if it's greater than the current value
+        for (int j = index + 1; j < length; j++){
+            if (temperatures[j] > temperatures[index]){
+                recurseSpanLength(j, spanLength + 1, spanUpTo, temperatures);
+            }
+        }
+
     }
 }
