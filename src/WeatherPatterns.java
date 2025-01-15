f7a3b1bc5ce8f398097801602eaca54767ccdf51
kate <kate.little@menloschool.org>
2025-01-15 10:40:04 -0800

    1/15 - debugging

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -15,16 +15,19 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
         int length = temperatures.length;
         int[] spanUpTo = new int[length];
-        // Represents the highest temperature that has serves as the start of a span of days
-        int startMax = -10000;
+        // Represents the highest temperature that has served as the start of a span of days
+        int startMax = 1000;
         // Each position/temp in the array should be a potential start
         for (int i = 0; i < length; i++){
             // Only start there if it's lower than the highest previous start
             if (temperatures[i] < startMax){
-                recurseSpanLength(i, 0, spanUpTo, temperatures);
+                recurseSpanLength(i, 1, spanUpTo, temperatures);
+            }
+            // Update startMax
+            if (temperatures[i] > startMax){
+                startMax = temperatures[i];
             }
         }
         int maxRun = 0;
@@ -39,14 +42,14 @@ public class WeatherPatterns {
     // Calculates the distance it takes to get to a certain temp
     // Follows an algorithm like moksha patam!
     public static void recurseSpanLength(int index, int spanLength, int[] spanUpTo, int[] temperatures){
-        // Base case: if final temperature has been reached
-        int length = temperatures.length;
-        if (index == length - 1){
-            return;
-        }
         // Only update span length if it's greater than what's currently listed
         if (spanLength > spanUpTo[index]){
             spanUpTo[index] = spanLength;
+        }
+        // Base case: if final temperature has been reached, update span length & return
+        int length = temperatures.length;
+        if (index == length - 1){
+            return;
         }
         // Recurse for each subsequent spot on the board, but only if it's greater than the current value
         for (int j = index + 1; j < length; j++){
@@ -54,6 +57,5 @@ public class WeatherPatterns {
                 recurseSpanLength(j, spanLength + 1, spanUpTo, temperatures);
             }
         }
-
     }
 }
